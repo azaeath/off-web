402e1714b83391d67640a9fafa4f6507d57947b2
azaeath <azaeath@users.noreply.github.com>
2019-11-12 19:14:02 -0600

    Gallery and Structure Update
    Change the file folder structure to have all css files in the css folder and all js files in their own folder.
    
    The image gallery is currently further along with this update, as it has a "light box" modal slideshow thing now. There's too many names for it. It be crazy. Anyway, it's not just a bland lifeless page of boxes. It's now a page of boxes and images that need to be re-styled.

--- tog.js
+++
@@ -1,9 +0,0 @@
-// Toggle buttons and their functions. Hopefully, this goes well as my first attempt at JS.// Toggle buttons and their functions. Hopefully, this goes well as my first attempt at JS.
-function mobiMenu() {
-	var x = document.getElementById("mobiNav");
-	if (x.style.left === "-100%") {
-		x.style.left = "0";
-	} else {
-		x.style.left = "-100%";
-	}
-}
