656fd3d3309587cc27ef29b07e44d35bbbccd4b7
azaeath <azaeath@users.noreply.github.com>
2021-03-17 01:45:24 -0500

    cleanUp
    Getting rid of old unnecessary web files.

--- js/tog.js
+++ js/tog.js
@@ -7,3 +7,21 @@ function mobiMenu() {
 		x.style.left = "-100%";
 	}
 }
+
+function openTab(coInfo, coType) {
+  // Declare all variables
+  var i, tabCont, tabLink;
+  // Get all elements with class="tabCont" and hide them
+  tabCont = document.getElementsByClassName("tabCont");
+  for (i = 0; i < tabCont.length; i++) {
+    tabCont[i].style.display = "none";
+  }
+  // Get all elements with class="tabLink" and remove the class "active"
+  tabLink = document.getElementsByClassName("tabLink");
+  for (i = 0; i < tabLink.length; i++) {
+    tabLink[i].className = tabLink[i].className.replace(" active", "");
+  }
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(coType).style.display = "block";
+  coInfo.currentTarget.className += " active";
+}
